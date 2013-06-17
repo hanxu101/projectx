5b32dca3d4570a208188b29c9ac2d0caa9c5fa75
hanxu101 <xodennisxo@hotmail.com>
2013-06-16 23:36:45 -0700

    [HX]  - Set Screen Orientation to portrait on win32. For iOS&Android, please see: http://www.cocos2d-x.org/boards/6/topics/14072

--- samples/Cpp/ProjectX/proj.win32/main.cpp
+++ samples/Cpp/ProjectX/proj.win32/main.cpp
@@ -21,7 +21,14 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 #else
     eglView->setViewName("ProjectX");
 #endif
+
+    // Simply swap width and height for Win32. For other platform, see: http://www.cocos2d-x.org/boards/6/topics/14072
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
+    eglView->setFrameSize(1536, 2048);
+#else
     eglView->setFrameSize(2048, 1536);
+#endif
+
     // The resolution of ipad3 is very large. In general, PC's resolution is smaller than it.
     // So we need to invoke 'setFrameZoomFactor'(only valid on desktop(win32, mac, linux)) to make the window smaller.
     eglView->setFrameZoomFactor(0.4f);
